8e4ef1a4f34e5fccbc5c3a0828dcf2d574746462
OMAsphyxiate <omasphyxiate@gmail.com>
2017-06-06 16:13:02 -0500

    Added print to List Tutorial to see result

--- list_tutorial.py
+++ list_tutorial.py
@@ -13,10 +13,11 @@ string_example.split('-') #results in a printed list of 5 separate words
 #We can store these results by defining it as another variable
 
 string_list = string_example.split('-')
+print(string_list)
 
 #We can add to the list by using the .append method
 numbers = [1,2,3,4,5]
-number.append(6) #Will add the number 6 to the end of the list
+numbers.append(6) #Will add the number 6 to the end of the list
 
 #If you wanted to add a set # of items into the list, you can use a for loop
 numberlist=[]
@@ -24,7 +25,7 @@ for i in range(100):
 	numberlist.append(i)
 
 #To see what a potential range will generate, you can use the following
-list(range(10))
+print (list(range(10)))
 #Which will show [0,1,2,3,4,5,6,7,8,9]
 #So if you wanted a specific range of numbers to be added such as 6 to 30...
 for i in range(6,31): #Since the range will begin at the 6 and end before 31
